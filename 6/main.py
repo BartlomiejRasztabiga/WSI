10d6e8b82da08b25816c956e298675928b88da52
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2022-01-24 18:04:23 +0100

    Final code?

--- 6/main.py
+++ 6/main.py
@@ -1,23 +1,29 @@
 import gym
-import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
 from numpy.random import default_rng
 
+ENV_NAME = 'Taxi-v3'
 
-def run(episodes=2000, learning_rate=0.03, discount_rate=0.97, exploration_rate=1.0):
+
+def draw_chart(x_axis, y_axis, title, file_name, x_label, y_label):
+    plt.plot(x_axis, y_axis)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.xticks(x_axis)
+    plt.title(title)
+    plt.savefig(file_name)
+    plt.close()
+
+
+def train(episodes, learning_rate, discount_rate, exploration_rate):
     rng = default_rng(seed=1)
-    env = gym.make('Taxi-v3')
+    env = gym.make(ENV_NAME)
 
-    actions_space_size = env.action_space.n
-    observations_space_size = env.observation_space.n
-
-    q_table = np.zeros((observations_space_size, actions_space_size))
-
-    rewards = []
+    q_table = np.zeros((500, 6))
 
     for episode in range(episodes):
         state = env.reset()
-        episode_reward = 0
 
         while True:
             rnd = rng.uniform()
@@ -31,62 +37,98 @@ def run(episodes=2000, learning_rate=0.03, discount_rate=0.97, exploration_rate=
                     reward + discount_rate * np.max(q_table[new_state, :]) - q_table[state, action])
 
             state = new_state
+
+            if done:
+                break
+
+    return q_table
+
+
+def evaluate(q_table, episodes):
+    env = gym.make(ENV_NAME)
+    rewards = []
+
+    for episode in range(episodes):
+        state = env.reset()
+        episode_reward = 0
+
+        while True:
+            action = np.argmax(q_table[state, :])
+            new_state, reward, done, _ = env.step(action)
+            state = new_state
             episode_reward += reward
 
             if done:
                 rewards.append(episode_reward)
                 break
 
-    plt.plot(range(episodes), rewards)
-    plt.title('Rewards')
-    plt.grid()
-    plt.savefig(f"episodes={episodes} lr={learning_rate} dr={discount_rate} er={exploration_rate}.png")
-    plt.close()
-
-    return np.mean(rewards[-100:])
+    return np.mean(rewards)
 
 
 def main():
-    optimal_episodes = 5000
-    optimal_lr = 0.03
-    optimal_dr = 0.97
-    optimal_er = 0.001
+    eval_episodes = 100
+    optimal_episodes = 4000
+    optimal_lr = 0.2
+    optimal_dr = 1.0
+    optimal_er = 0.5
+
+    ER = [0.5, 0.1, 0.01, 0.001, 0.0001]
+    EPISODES = [1000, 2000, 5000, 10000]
+    LR = [1.0, 0.8, 0.6, 0.4, 0.2, 0.1, 0.01]
+    DR = [1.0, 0.8, 0.6, 0.4, 0.2, 0.1, 0.01]
 
     print("comparing ER")
-    for exploration_rate in [0.5, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001]:
-        avg_reward = run(episodes=optimal_episodes,
-                         learning_rate=optimal_lr,
-                         discount_rate=optimal_dr,
-                         exploration_rate=exploration_rate)
+    x = []
+    y = []
+    for exploration_rate in ER:
+        q_table = train(episodes=optimal_episodes,
+                        learning_rate=optimal_lr,
+                        discount_rate=optimal_dr,
+                        exploration_rate=exploration_rate)
+        avg_reward = evaluate(q_table, eval_episodes)
+        x.append(exploration_rate)
+        y.append(avg_reward)
         print(exploration_rate, avg_reward)
-    print()
+    draw_chart(x, y, 'Porównanie ER', 'er_comparison.png', 'exploration rate', 'średnia nagroda')
 
     print("comparing episodes")
-    for episode in [100, 1000, 2000, 5000, 10000]:
-        avg_reward = run(episodes=episode,
-                         learning_rate=optimal_lr,
-                         discount_rate=optimal_dr,
-                         exploration_rate=optimal_er)
+    x = []
+    y = []
+    for episode in EPISODES:
+        q_table = train(episodes=episode,
+                        learning_rate=optimal_lr,
+                        discount_rate=optimal_dr,
+                        exploration_rate=optimal_er)
+        avg_reward = evaluate(q_table, eval_episodes)
+        x.append(episode)
+        y.append(avg_reward)
         print(episode, avg_reward)
-    print()
 
     print("comparing LR")
-    for lr in [2.0, 1.0, 0.5, 0.1, 0.01, 0.001]:
-        avg_reward = run(episodes=optimal_episodes,
-                         learning_rate=lr,
-                         discount_rate=optimal_dr,
-                         exploration_rate=optimal_er)
+    x = []
+    y = []
+    for lr in LR:
+        q_table = train(episodes=optimal_episodes,
+                        learning_rate=lr,
+                        discount_rate=optimal_dr,
+                        exploration_rate=optimal_er)
+        avg_reward = evaluate(q_table, eval_episodes)
+        x.append(lr)
+        y.append(avg_reward)
         print(lr, avg_reward)
-    print()
 
     print("comparing DR")
-    for dr in [1.0, 0.5, 0.1, 0.01, 0.001, 0.0001]:
-        avg_reward = run(episodes=optimal_episodes,
-                         learning_rate=optimal_lr,
-                         discount_rate=dr,
-                         exploration_rate=optimal_er)
+    x = []
+    y = []
+    for dr in DR:
+        q_table = train(episodes=optimal_episodes,
+                        learning_rate=optimal_lr,
+                        discount_rate=dr,
+                        exploration_rate=optimal_er)
+        avg_reward = evaluate(q_table, eval_episodes)
+        x.append(dr)
+        y.append(avg_reward)
         print(dr, avg_reward)
-    print()
 
 
 if __name__ == "__main__":
